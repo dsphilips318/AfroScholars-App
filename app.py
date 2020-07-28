be48e2cf3cde20d1e9ca7627bf73fe43f5bb9284
dsphilips318 <dsphilips18@gmail.com>
2020-07-28 11:39:24 -0400

    Additional Pages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask
 from flask import render_template
-from flask import request
+# from flask import request
 
 
 # -- Initialization section --
@@ -14,3 +14,23 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/languages')
+def languages():
+    return render_template('languages.html')
+
+
+@app.route('/yoruba-demo')
+def yoruba_demo():
+    return render_template('yoruba-demo.html')
+
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign-up.html')
+
+
+@app.route('/log-in')
+def log_in():
+    return render_template('log-in.html')
