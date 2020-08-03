0d19429061b055ed5e605918b38fda96d1b6abe6
dsphilips318 <dsphilips18@gmail.com>
2020-08-03 17:13:07 +0000

    Second Lesson Complete

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from model import feedback
+from model import feedback1
+from model import feedback2
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -43,49 +44,49 @@ def lesson1_question1():
     return render_template('yoruba-lesson1_copy/questions.html')
 
 
-@app.route('/yoruba/lesson1/question2')
-def lesson1_question2():
-    return render_template('yoruba-lesson1/q2.html')
+# @app.route('/yoruba/lesson1/question2')
+# def lesson1_question2():
+#     return render_template('yoruba-lesson1/q2.html')
 
 
-@app.route('/yoruba/lesson1/question3')
-def lesson1_question3():
-    return render_template('yoruba-lesson1/q3.html')
+# @app.route('/yoruba/lesson1/question3')
+# def lesson1_question3():
+#     return render_template('yoruba-lesson1/q3.html')
 
 
-@app.route('/yoruba/lesson1/question4')
-def lesson1_question4():
-    return render_template('yoruba-lesson1/q4.html')
+# @app.route('/yoruba/lesson1/question4')
+# def lesson1_question4():
+#     return render_template('yoruba-lesson1/q4.html')
 
 
-@app.route('/yoruba/lesson1/question5')
-def lesson1_question5():
-    return render_template('yoruba-lesson1/q5.html')
+# @app.route('/yoruba/lesson1/question5')
+# def lesson1_question5():
+#     return render_template('yoruba-lesson1/q5.html')
 
 
-@app.route('/yoruba/lesson1/question6')
-def lesson1_question6():
-    return render_template('yoruba-lesson1/q6.html')
+# @app.route('/yoruba/lesson1/question6')
+# def lesson1_question6():
+#     return render_template('yoruba-lesson1/q6.html')
 
 
-@app.route('/yoruba/lesson1/question7')
-def lesson1_question7():
-    return render_template('yoruba-lesson1/q7.html')
+# @app.route('/yoruba/lesson1/question7')
+# def lesson1_question7():
+#     return render_template('yoruba-lesson1/q7.html')
 
 
-@app.route('/yoruba/lesson1/question8')
-def lesson1_question8():
-    return render_template('yoruba-lesson1/q8.html')
+# @app.route('/yoruba/lesson1/question8')
+# def lesson1_question8():
+#     return render_template('yoruba-lesson1/q8.html')
 
 
-@app.route('/yoruba/lesson1/question9')
-def lesson1_question9():
-    return render_template('yoruba-lesson1/q9.html')
+# @app.route('/yoruba/lesson1/question9')
+# def lesson1_question9():
+#     return render_template('yoruba-lesson1/q9.html')
 
 
-@app.route('/yoruba/lesson1/question10')
-def lesson1_question10():
-    return render_template('yoruba-lesson1/q10.html')
+# @app.route('/yoruba/lesson1/question10')
+# def lesson1_question10():
+#     return render_template('yoruba-lesson1/q10.html')
 
 
 @app.route('/yoruba/lesson1/results', methods=['GET', 'POST'])
@@ -115,7 +116,7 @@ def lesson1_results():
             'answer9': answer9,
             'answer10': answer10
         }
-        results = feedback(responses)
+        results = feedback1(responses)
         percentage = results.get('percentage')
         message = results.get('message')
         props = {
@@ -132,51 +133,83 @@ def lesson2_question1():
     return render_template('yoruba-lesson2_copy/questions.html')
 
 
-@app.route('/yoruba/lesson2/question2')
-def lesson2_question2():
-    return render_template('yoruba-lesson2/q2.html')
+# @app.route('/yoruba/lesson2/question2')
+# def lesson2_question2():
+#     return render_template('yoruba-lesson2/q2.html')
 
 
-@app.route('/yoruba/lesson2/question3')
-def lesson2_question3():
-    return render_template('yoruba-lesson2/q3.html')
+# @app.route('/yoruba/lesson2/question3')
+# def lesson2_question3():
+#     return render_template('yoruba-lesson2/q3.html')
 
 
-@app.route('/yoruba/lesson2/question4')
-def lesson2_question4():
-    return render_template('yoruba-lesson2/q4.html')
+# @app.route('/yoruba/lesson2/question4')
+# def lesson2_question4():
+#     return render_template('yoruba-lesson2/q4.html')
 
 
-@app.route('/yoruba/lesson2/question5')
-def lesson2_question5():
-    return render_template('yoruba-lesson2/q5.html')
+# @app.route('/yoruba/lesson2/question5')
+# def lesson2_question5():
+#     return render_template('yoruba-lesson2/q5.html')
 
 
-@app.route('/yoruba/lesson2/question6')
-def lesson2_question6():
-    return render_template('yoruba-lesson2/q6.html')
+# @app.route('/yoruba/lesson2/question6')
+# def lesson2_question6():
+#     return render_template('yoruba-lesson2/q6.html')
 
 
-@app.route('/yoruba/lesson2/question7')
-def lesson2_question7():
-    return render_template('yoruba-lesson2/q7.html')
+# @app.route('/yoruba/lesson2/question7')
+# def lesson2_question7():
+#     return render_template('yoruba-lesson2/q7.html')
 
 
-@app.route('/yoruba/lesson2/question8')
-def lesson2_question8():
-    return render_template('yoruba-lesson2/q8.html')
+# @app.route('/yoruba/lesson2/question8')
+# def lesson2_question8():
+#     return render_template('yoruba-lesson2/q8.html')
 
 
-@app.route('/yoruba/lesson2/question9')
-def lesson2_question9():
-    return render_template('yoruba-lesson2/q9.html')
+# @app.route('/yoruba/lesson2/question9')
+# def lesson2_question9():
+#     return render_template('yoruba-lesson2/q9.html')
 
 
-@app.route('/yoruba/lesson2/question10')
-def lesson2_question10():
-    return render_template('yoruba-lesson2/q10.html')
+# @app.route('/yoruba/lesson2/question10')
+# def lesson2_question10():
+#     return render_template('yoruba-lesson2/q10.html')
 
 
-@app.route('/yoruba/lesson2/results')
+@app.route('/yoruba/lesson2/results', methods=['GET', 'POST'])
 def lesson2_results():
-    return render_template('yoruba-lesson2/results.html')
+    if request.method == 'GET':
+        return "You are getting the results page"
+    if request.method == 'POST':
+        answer1 = request.form['answer_1']
+        answer2 = request.form['answer_2']
+        answer3 = request.form['answer_3']
+        answer4 = request.form['answer_4']
+        answer5 = request.form['answer_5']
+        answer6 = request.form['answer_6']
+        answer7 = request.form['answer_7']
+        answer8 = request.form['answer_8']
+        answer9 = request.form['answer_9']
+        answer10 = request.form['answer_10']
+        responses = {
+            'answer1': answer1,
+            'answer2': answer2,
+            'answer3': answer3,
+            'answer4': answer4,
+            'answer5': answer5,
+            'answer6': answer6,
+            'answer7': answer7,
+            'answer8': answer8,
+            'answer9': answer9,
+            'answer10': answer10
+        }
+        results = feedback2(responses)
+        percentage = results.get('percentage')
+        message = results.get('message')
+        props = {
+            'percentage': percentage,
+            'message': message
+        }
+        return render_template('yoruba-lesson2_copy/results.html', props=props)
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-answers = {
+answers_1 = {
     'answer1': 'Ookan',
     'answer2': 'Eeji',
     'answer3': 'Eeta',
@@ -11,44 +11,70 @@ answers = {
     'answer10': 'Five'
 }
 
-props_test = {
-    'answer1': 'Ookan',
-    'answer2': 'Eeji',
-    'answer3': 'Eeta',
-    'answer4': 'Eerin',
-    'answer5': 'Aarun',
-    'answer6': 'One',
-    'answer7': 'Two',
+answers_2 = {
+    'answer1': 'Eefa',
+    'answer2': 'Eeje',
+    'answer3': 'Eejo',
+    'answer4': 'Eesan',
+    'answer5': 'Eewa',
+    'answer6': 'Ten',
+    'answer7': 'Seven',
     'answer8': 'Six',
-    'answer9': 'Four',
-    'answer10': 'Five'
+    'answer9': 'Nine',
+    'answer10': 'Eight'
 }
 
 
-def getGrade(props):
+def getGrade1(props):
     answers_right = 0
     for answer in props:
-        if props[answer] == answers[answer]:
+        if props[answer] == answers_1[answer]:
             answers_right += 1
     return answers_right
 
 
-feedback_message = {
+def getGrade2(props):
+    answers_right = 0
+    for answer in props:
+        if props[answer] == answers_2[answer]:
+            answers_right += 1
+    return answers_right
+
+
+feedback_message1 = {
+    'percentage': '',
+    'message': ''
+}
+
+feedback_message2 = {
     'percentage': '',
     'message': ''
 }
 
 
-def feedback(props):
-    questions_right = getGrade(props)
+def feedback1(props):
+    questions_right = getGrade1(props)
     # percent = f'{str(questions_right*10)}%'
     percent = str(questions_right*10) + '%'
-    feedback_message.update({'percentage': percent})
+    feedback_message1.update({'percentage': percent})
     if questions_right >= 8:
-        feedback_message.update({'message': f'Wow! You scored a {percent}. Great Job! Come back again soon for more practice.'})
+        feedback_message1.update({'message': f'Wow! You scored a {percent}. Great Job! Come back again soon for more practice.'})
     if questions_right >= 6 and questions_right <= 7:
-        feedback_message.update({'message': f'You scored a {percent}. Not bad. Practice this lesson more and you can get an even better score.'})
+        feedback_message1.update({'message': f'You scored a {percent}. Not bad. Practice this lesson more and you can get an even better score.'})
     if questions_right < 6:
-        feedback_message.update({'message': f'Unfortunately, you scored a {percent}. Don\'t worry. Practice this lesson again and again and you can get an even better score.'})
-    return feedback_message
+        feedback_message1.update({'message': f'Unfortunately, you scored a {percent}. Don\'t worry. Practice this lesson again and again and you can get an even better score.'})
+    return feedback_message1
 
+
+def feedback2(props):
+    questions_right = getGrade2(props)
+    # percent = f'{str(questions_right*10)}%'
+    percent = str(questions_right*10) + '%'
+    feedback_message2.update({'percentage': percent})
+    if questions_right >= 8:
+        feedback_message2.update({'message': f'Wow! You scored a {percent}. Great Job! Come back again soon for more practice.'})
+    if questions_right >= 6 and questions_right <= 7:
+        feedback_message2.update({'message': f'You scored a {percent}. Not bad. Practice this lesson more and you can get an even better score.'})
+    if questions_right < 6:
+        feedback_message2.update({'message': f'Unfortunately, you scored a {percent}. Don\'t worry. Practice this lesson again and again and you can get an even better score.'})
+    return feedback_message2
